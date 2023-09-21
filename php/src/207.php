31099a3702a0b6b9466b2830e26db93ec360f356
JavierGuijarro <javierguijarroverdu@gmail.com>
2023-09-21 17:10:31 +0200

    [MOD] Ejercicio 207 php comentado

--- php/src/207.php
+++ php/src/207.php
@@ -11,6 +11,7 @@
         $numero1 = $_GET["numero1"];
         $numero2 = $_GET["numero2"];
         $numero3 = $_GET["numero3"];
+        //max mejor opción
         $mayor = ($numero1 > $numero2) ? (($numero1 > $numero3) ? 
         $numero1 : $numero3) : (($numero2 > $numero3) ? $numero2 : $numero3);
         echo "El nombre més gran entre $numero1, $numero2 i $numero3 és el $mayor";
