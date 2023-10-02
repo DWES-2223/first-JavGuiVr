5d478873efa24a18f6e847eb9bc8df0adff37e37
JavierGuijarro <javierguijarroverdu@gmail.com>
2023-10-02 12:05:19 +0200

    Métodos del functions creados

--- php/src/functions.php
+++ php/src/functions.php
@@ -86,3 +86,61 @@ function peseta2euros(float $quant, float $cotizacio = 0.0060): float {
 function euro2pesetes(float $quant, float $cotizacio = 166.386): float {
     return $quant * $cotizacio;
 }
+
+function posicionsImparelles(string $frase): string {
+    $fraseFinal = "";
+    for ($i = 1; $i < strlen($frase); $i += 2) {
+        $fraseFinal .= substr($frase, $i, 1);
+    }
+    return $fraseFinal;
+}
+
+function quantitatVocals(string $frase) {
+    $totalA = substr_count($frase, "a");
+    $totalE = substr_count($frase, "e");
+    $totalI = substr_count($frase, "i");
+    $totalO = substr_count($frase, "o");
+    $totalU = substr_count($frase, "u");
+    $totalVocals = $totalA + $totalE + $totalI + $totalO + $totalU;
+    echo "Total vocals A: ". $totalA;
+    echo "Total vocals E: ". $totalE;
+    echo "Total vocals I: ". $totalI;
+    echo "Total vocals O: ". $totalO;
+    echo "Total vocals U: ". $totalU;
+    echo "Total vocals: ". $totalVocals;
+}
+
+function paraulesSeparades(string $frase) {
+    $paraules = explode(" ", $frase);
+    $numParaules = count($paraules);
+    echo "Total de paraules: $numParaules \n";
+
+    $paraulesJuntes = str_replace($frase," ", "");
+    $quantitatLletres = strlen($paraulesJuntes);
+    echo "Total de lletres: $quantitatLletres \n";
+
+    foreach ($paraules as $paraula) {
+        echo "Longitud de la paraula ". $paraula. ": ". strlen($paraula). "\n";
+    }
+}
+
+function transformarCadena(string $cadena) {
+    $cadenaFinal = "";
+    for ($i = 0; $i < strlen($cadena); $i++) {
+        if ($i % 2 == 0) {
+            $cadenaFinal .= substr($cadena, $i, 1);
+        } else {
+            $cadenaFinal .= strtoupper(substr($cadena, $i, 1));
+        }
+    }
+    echo "$cadenaFinal";
+}
+
+
+function esPalindrome(string $frase) {
+
+}
+
+function caractersCadena(string $frase) {
+
+}
