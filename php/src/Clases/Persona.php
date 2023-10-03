d0bf442b95b1d25e199cc081e9590f4dca21d6a6
JavierGuijarro <javierguijarroverdu@gmail.com>
2023-10-03 19:50:04 +0200

    [ADD] Ejercicios 301, 302 y 333 clases añadidos

---
+++ php/src/Clases/Persona.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Clases;
+class Persona
+{
+    private string $nom;
+    private string $cognoms;
+    private int $edad;
+
+    public function __construct(string $nom, string $cognoms, int $edad)
+    {
+        $this->nom = $nom;
+        $this->cognoms = $cognoms;
+        $this->edad = $edad;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getCognoms(): string
+    {
+        return $this->cognoms;
+    }
+
+    public function setCognoms(string $cognoms): void
+    {
+        $this->cognoms = $cognoms;
+    }
+
+    public function getEdad(): int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(int $edad): void
+    {
+        $this->edad = $edad;
+    }
+
+    function getNombreCompleto(): string {
+        return $this->getNom() . $this->getCognoms();
+    }
+
+    function estaJubilat(): bool {
+        return $this->edad >= 67 ? true : false;
+    }
+}
---
+++ php/src/Clases/Persona8.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Clases;
+class Persona8
+{
+    public function __construct(
+        private string $nom,
+        private string $cognoms,
+        private int $edad = 25,
+    ) {}
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function getCognoms(): string
+    {
+        return $this->cognoms;
+    }
+
+    public function setCognoms(string $cognoms): void
+    {
+        $this->cognoms = $cognoms;
+    }
+
+    public function getEdad(): int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(int $edad): void
+    {
+        $this->edad = $edad;
+    }
+
+    function getNombreCompleto(): string {
+        return $this->getNom() . $this->getCognoms();
+    }
+
+    function estaJubilat(): bool {
+        return $this->edad >= 67 ? true : false;
+    }
+}
